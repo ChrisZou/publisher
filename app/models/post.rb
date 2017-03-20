a30997f5addff6cc900a8be9c808c1775a68fcf3
xiaochuang <xiaochuang@mogujie.com>
2017-03-20 16:16:25 +0800

    fenced code block

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 require 'redcarpet'
 class Post < ApplicationRecord
   def render_markdown
-    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true, fenced_code_blocks: true)
     markdown.render(self.content || '')
   end
 end
